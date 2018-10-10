d4ade013224b189981c7f28655b37558679a20ed
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-10-10 13:12:46 -0400

    Misc

--- bitcoinav.js
+++ bitcoinav.js
@@ -1,6 +1,5 @@
-let currentDate=dateNow();
 let APIkey='OWE2NmUyZDk5MDllNGUwYWJkZWJiYTlhYWQ1ZDVjZjc'
-let queryString=`${currentDate}.${APIkey}`
+let queryString=`${Date.now()}.${APIkey}`
 let secretApi='MmQ3NzMzZDQzNDIxNDIyZjk5MWFhMmU0MjQ4MzVmMTM0NjgyZTNmMzcwMGE0YzZhYmNmOWM1NWU3MDYyNGI1ZQ'
 const queryUrl=`https://apiv2.bitcoinaverage.com/indices/global/ticker/all?crypto={crypto}&fiat={fiat}`
 
@@ -10,5 +9,5 @@ const getBit=function(){
         method: 'GET'
     }).then(function(response){
 
-    }
+    });
 }
