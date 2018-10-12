145399fbfad27826615049906c899f563135083d
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-10-12 02:02:57 -0400

    Display and update chart dynamically
    Add function that updates the chart, inputting new data from the
    Bitcoin Average API and applying changes to the base currency, quote
    currency, or time period.
    
    Create function that initiates an auto updater using any time interval,
    and function that stops the auto updater if it is active
    
    Rename files and move around code for organization, readability, and logic.
    Missing inline documentation.

---
+++ app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    setParams('BTC', 'USD', 'monthly');
+    updateChart();
+});
---
+++ chart-data.js
@@ -0,0 +1,54 @@
+let base;
+let quote;
+let period;
+
+function getQueryURL() {
+    return `https://apiv2.bitcoinaverage.com/indices/global/history/${base}${quote}?period=${period}&format=json`;
+}
+
+function setParams(newBase, newQuote, newPeriod) {
+    base = newBase || base;
+    quote = newQuote || quote;
+    period = newPeriod || period;
+}
+
+function getChartData(cb) {
+    callBitcoinAvgAPI(getQueryURL(), function (response) {
+        const differentTimes = myChart.data.labels[0] !== response[response.length - 1].time;
+        const differentAverages = myChart.data.datasets[0].data[0] !== response[response.length - 1].average;
+        if (differentTimes || differentAverages) {
+            console.log('updated!')
+            let chartData = {
+                times: [],
+                averages: [],
+            };
+            for (let i = response.length - 1; i >= 0; i--) {
+                chartData.times.push(response[i].time);
+                chartData.averages.push(response[i].average);
+            }
+            cb(chartData);
+        } else {
+            console.log('not updated')
+        }
+    })
+}
+
+function updateChart() {
+    getChartData(function(chartData) {
+        console.log('Old latest timestamp: ' + myChart.data.labels[myChart.data.labels.length - 1]);
+        myChart.data.labels = chartData.times;
+        myChart.data.datasets[0].data = chartData.averages;
+        myChart.options.title.text = base + quote;
+        myChart.update();
+        console.log('New latest timestamp: ' + myChart.data.labels[myChart.data.labels.length - 1]);
+    });
+}
+
+let autoUpdater;
+function startAutoUpdater(time) {
+    clearInterval(autoUpdater);
+    autoUpdater = setInterval(updateChart, time);
+}
+function stopAutoUpdater() {
+    clearInterval(autoUpdater);
+}
