d2f646477352eb30466cab8822d021934be5541c
walkerSwift <walker@empirex.tech>
2018-10-15 12:46:38 -0400

    added conversion functionality and edited coin values

--- app.js
+++ app.js
@@ -2,38 +2,3 @@ $(document).ready(function() {
     setParams('BTC', 'USD', 'monthly'); // sets query parameters for Bitcoin Average API
     updateChart(); // renders the chart
 });
-
-
-//dynamically adding the currencys to the drop down menu:
-
-const renderCurrencies = function (){
-    $(".dropdown").empty();
-    for(let i = 0; i < currencies.length; i++){
-        const currencyBtn = $("<option>");
-        currencyBtn.text(currencies[i]);
-        $(".dropdown").append(currencyBtn);
-    }
-}
-
-//to see if a currency is a pair:
-
-// const pairCheck = function () {
-//     for(let i = 0; i < )
-// }
-
-
-//currency converter:
-
-// const currencyConverter = function (){
-//     var data = JSON.stringify(jqXHR.responseText)
-//     let quoteAmount = baseAmount /data.fiat
-    
-// }
-
-// const orderSubmit = function () {
-//     if(response[i].code)
-// }
-
-// $(".submit").on("click", orderSubmit);
-
-
--- exchange-api.js
+++ exchange-api.js
@@ -1,150 +1,134 @@
-const getPrice = function () {
-    const priceUrl = "https://api.nexchange.io/en/api/v1/get_price/pair_name/?amount_base=1.24354147&amount_quote=100";
-    const paramters = {
-        "pair_name": pairName,
-        "amount_base": baseAmount,
-        "amount_quote": quoteAmount,
-        };
-
-        //quoteAmount is the amount sent
-        //baseAmount is amount of currency you recieve in exchange for the amountQuote
-
-        const pairName = $("").val();
-        if (pairName) {
-            paramters.pair_name = pairName;
-        }
-        const baseAmount = $(".firstCurrency").val();
-        if (baseAmount) {
-            paramters.amount_base = baseAmount;
-        }
-        const quoteAmount = $("").val();
-        if (pairName) {
-            paramters.amount_quote = quoteAmount;
-        }
-
-        priceUrl += '?' + $.param(paramters);
-
-        console.log(priceUrl);
-}
-
 function handleError(jqXHR) {
-    console.log(jqXHR.resposneText);
+  console.log(JSON.parse(jqXHR.responseText));
 }
 
-const currencies = [];
+function cbCurrency(data) {
+  console.log(data);
 
-$.ajax({
+  $.ajax({
     type: "GET",
-    url: "https://api.nexchange.io/en/api/v1/currency/",
-}).then(function(response){
-    for(let i = 0; i < response.length; i++){
-        console.log(response[i].code);
-        currencies.push(response[i].code)
+    url: "https://api.nexchange.io/en/api/v1/pair/"
+  }).then(function(response) {
+    for (let i = 0; i < response.length; i++) {
+      console.log(response[i].name);
+      if (response[i].base != response[i].quote) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
     }
-    console.log(currencies);
-    renderCurrencies();
-});
-
-
-const baseCoin = $(".coin1").val();
-const quoteCoin = $(".coin2").val();
-
-function cbCurrency(data){
-    console.log(data);
-
-    $.ajax({
-        type: "GET",
-        url: "https://api.nexchange.io/en/api/v1/pair/",
-    }).then(function(response){
-        for(let i = 0; i < response.length; i++){
-            console.log(response[i].name);
-            if(response[i].base != response[i].quote){
-                console.log(true);
-            } else {
-                console.log(false);
-            }
-        }
-    });
+  });
 }
 
-
-function cbPairs(data){
-    console.log(data);
+function cbPairs(data) {
+  console.log(data);
 }
 
 const createOrder = function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const orderURL = `https://api.nexchange.io/en/api/v1/orders/`
+  const orderURL = `https://api.nexchange.io/en/api/v1/orders/`;
 
-    let askingFee;
-    let biddingFee;
-    let coinName;
-    let baseCode;
-    let quoteCode;
-    let type;
-    let coinAddy;
-    let currencyCode;
+  let askingFee;
+  let biddingFee;
+  let coinName;
+  let baseCode;
+  let quoteCode;
+  let type;
+  let coinAddy;
+  let currencyCode;
 
-    // coinName = 'ETHUSD';
-    // baseCode = 'ETH';
-    // quoteCode = 'USD';
-    type = 'W';
-    // coinAddy = '0xfCc2FeedEd9d3503217B9c0e1ce987B4B84DB2b5';
+  // coinName = 'ETHUSD';
+  // baseCode = 'ETH';
+  // quoteCode = 'USD';
+  type = "W";
+  coinAddy = "0xfCc2FeedEd9d3503217B9c0e1ce987B4B84DB2b5";
 
-    coinAddy = $('.address').val();
-    quoteCode = $(".dropdown").val();
-    baseCode = $("#dropTwo").val();
-    coinName = baseCode + quoteCode;
-    
+  // coinAddy = $('.address').val();
+  quoteCode = $(".quoteSelect").val();
+  baseCode = $(".baseSelect").val();
+  coinName = baseCode + quoteCode;
 
-    const quoteAmount = $(".firstCurrency").val();
-    
-    console.log(coinName);
-    console.log(baseCode);
-    console.log(quoteCode);
+  const quoteAmount = $(".quoteInput").val();
 
-    const payload = {
-        "amount_quote": quoteAmount,
-        "is_default_rule": true,
-        "pair": {
-            "name": coinName,
-            "base": {
-                "code": baseCode,
-            },
-            "quote": {
-                "code": quoteCode, 
-            },
-            "fee_ask": {
-                "fee_ask": .001,
-            },
-            "fee_bid": {
-                "fee_bid": .001
-            }
-        },
-        "withdraw_address": {
-            "type": type,
-            "address": coinAddy,
-            "currency_code": 'ETH'
-        },
-    };
-    console.log(payload);
+  console.log(coinName);
+  console.log(baseCode);
+  console.log(quoteCode);
+
+  const payload = {
+    amount_quote: quoteAmount,
+    is_default_rule: true,
+    pair: {
+      name: coinName,
+      base: {
+        code: baseCode
+      },
+      quote: {
+        code: quoteCode
+      },
+      fee_ask: {
+        fee_ask: 0.001
+      },
+      fee_bid: {
+        fee_bid: 0.001
+      }
+    },
+    withdraw_address: {
+      type: type,
+      address: coinAddy,
+      currency_code: "ETH"
+    }
+  };
+  $;
+  console.log(payload);
+
+  $.ajax({
+    error: handleError,
+    url: orderURL,
+    method: "POST",
+    data: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+      "x-referral-token": null
+    }
+  }).then(function(response) {
+    console.log(response);
+  });
+};
+
+function convertPrice(e) {
+    const inputBox = $(e.target);
+
+    const quoteCode = $('.quoteSelect').val();
+    const baseCode = $('.baseSelect').val();
+    const pair = baseCode + quoteCode;
+
+    let quoteAmount;
+    let baseAmount;
+    let queryURL;
+
+    let inputtingQuote = inputBox.hasClass('quoteInput');
+    if (inputtingQuote) {
+        quoteAmount = inputBox.val();
+        queryURL = `https://api.nexchange.io/en/api/v1/get_price/${pair}/?amount_quote=${quoteAmount}`;
+    } else {
+        baseAmount = e.target.value;
+        queryURL = `https://api.nexchange.io/en/api/v1/get_price/${pair}/?amount_base=${baseAmount}`;
+    }
 
     $.ajax({
-        error: function(jqXHR) {
-            console.log(jqXHR.responseText);
-        },
-        url: orderURL,
-        method: "POST",
-        data: JSON.stringify(payload),
-        headers: {
-            "Content-Type": "application/json",
-            "x-referral-token": null
-        },
-    }).then(function(response) { 
-        console.log(response);
-    });
+        url: queryURL,
+        method: 'GET',
+        error: handleError,
+    }).then(function(response) {
+        if (inputtingQuote) {
+            $('.baseInput').val(response.amount_base);
+        } else {
+            $('.quoteInput').val(response.amount_quote);
+        }
+    })
 }
+$('.quoteInput, .baseInput').on('input', convertPrice);
 // createOrder();
-$('.submit').on('click', createOrder);
+$(".submit").on("click", createOrder);
 console.log(createOrder);
