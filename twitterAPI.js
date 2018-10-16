1dab92d1baae687755c47a956598caefbdd78ef6
Josh McCormick <josh.mccormick81@gmail.com>
2018-10-16 12:54:33 -0400

    fixed twitter API link

--- twitterAPI.js
+++ twitterAPI.js
@@ -16,7 +16,7 @@ let encodedBase = btoa(bearerCredens);
 console.log(`Basic ${encodedBase}`);
 const twitterAuth = "https://api.twitter.com/oauth2/token";
 const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
-const twitterTweets = "https://api.twitter.com/1.1/search/tweets.json?q=%23crypto+%23bitcoin"
+const twitterTweets = "https://api.twitter.com/1.1/search/tweets.json?q=%23crypto"
 $.ajax({
     url: corsAnywhere + twitterAuth,
     method: 'POST',
